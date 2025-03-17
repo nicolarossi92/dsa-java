17376d297c0d74e994878f40589af60349c7acb4
Nicola <ncl.rss.92@gmail.com>
2025-03-17 20:09:06 +0100

    C3ListAbstractionsTest introducing

---
+++ src/test/java/it/mauluk92/dsa/c3/C3ListAbstractionsTest.java
@@ -0,0 +1,15 @@
+package it.mauluk92.dsa.c3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class C3ListAbstractionsTest {
+
+    /**
+     * This chapter introduces concepts
+     * relative to the List ADT
+     */
+    @Test
+    @DisplayName("List abstractions")
+    public void listAbstractions(){}
+}
